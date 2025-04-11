bf6802296483849f6626bdeda0c54c00f802aaf1
Homodeus7 <plum.bus@yahoo.com>
2025-04-11 14:00:16 +0700

    Replaced columns for house plan

--- src/pages/housePlans/HousePlansPage.vue
+++ src/pages/housePlans/HousePlansPage.vue
@@ -1,54 +1,13 @@
 <script setup lang="ts">
-import type { HousePlanModel } from "@pages/housePlan/types/HousePlanModel";
 import { useHousesQuery } from "@pages/housePlans/api/useHousePlansQuery";
+import { columns } from "@pages/housePlans/table/columns";
 import { BaseTable } from "@shared/components";
-import type { ColumnDef } from "@tanstack/vue-table";
 import { ref } from "vue";
 
 const page = ref(1);
 const limit = 10;
 const { data, isLoading } = useHousesQuery(page, limit);
 
-const columns: ColumnDef<HousePlanModel>[] = [
-  {
-    accessorKey: "name",
-    header: "Название",
-    cell: (info) => info.getValue(),
-  },
-  {
-    accessorKey: "created_at",
-    header: "Создано",
-    cell: (info) => new Date(info.getValue() as string).toLocaleDateString(),
-  },
-  {
-    accessorKey: "updated_at",
-    header: "Обновлено",
-    cell: (info) => new Date(info.getValue() as string).toLocaleDateString(),
-  },
-  {
-    accessorKey: "entrances",
-    header: "Подъезды",
-    cell: (info) => {
-      const entrances = info.getValue() as unknown[];
-      const count = entrances?.length ?? 0;
-
-      const getLabel = (num: number) => {
-        const mod10 = num % 10;
-        const mod100 = num % 100;
-        if (mod10 === 1 && mod100 !== 11) return "подъезд";
-        if (mod10 >= 2 && mod10 <= 4 && (mod100 < 10 || mod100 >= 20))
-          return "подъезда";
-        return "подъездов";
-      };
-      return `${count ? count : ""} ${getLabel(count)}`;
-    },
-  },
-  {
-    id: "actions",
-    header: "Действия",
-    cell: () => "",
-  },
-];
 </script>
 
 <template>
@@ -62,6 +21,7 @@ const columns: ColumnDef<HousePlanModel>[] = [
       :page="page"
       :total="data.total"
       :limit="limit"
+      is-row-link
     />
 
     <div v-else-if="isLoading" class="text-gray-500">Загрузка...</div>
---
+++ src/pages/housePlans/table/columns.ts
@@ -0,0 +1,43 @@
+import type { HousePlanModel } from "@pages/housePlan/types/HousePlanModel";
+import type { ColumnDef } from "@tanstack/vue-table";
+
+export const columns: ColumnDef<HousePlanModel>[] = [
+  {
+    accessorKey: "name",
+    header: "Название",
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: "created_at",
+    header: "Создано",
+    cell: (info) => new Date(info.getValue() as string).toLocaleDateString(),
+  },
+  {
+    accessorKey: "updated_at",
+    header: "Обновлено",
+    cell: (info) => new Date(info.getValue() as string).toLocaleDateString(),
+  },
+  {
+    accessorKey: "entrances",
+    header: "Подъезды",
+    cell: (info) => {
+      const entrances = info.getValue() as unknown[];
+      const count = entrances?.length ?? 0;
+
+      const getLabel = (num: number) => {
+        const mod10 = num % 10;
+        const mod100 = num % 100;
+        if (mod10 === 1 && mod100 !== 11) return "подъезд";
+        if (mod10 >= 2 && mod10 <= 4 && (mod100 < 10 || mod100 >= 20))
+          return "подъезда";
+        return "подъездов";
+      };
+      return `${count ? count : ""} ${getLabel(count)}`;
+    },
+  },
+  {
+    id: "actions",
+    header: "Действия",
+    cell: () => "",
+  },
+];
